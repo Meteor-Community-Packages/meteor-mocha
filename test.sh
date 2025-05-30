7119ac06a8f52569bf8745fb04e5766276f5f630
jankapunkt <jkuester@uni-bremen.de>
2025-05-30 13:34:17 +0200

    tests: parameterized test script for future multi-browser driver tests

--- test.sh
+++ test.sh
@@ -3,10 +3,12 @@ echo "Setup package link"
 cd tests/dummy_app/ || exit 1
 meteor npm run setup
 
+DRIVER=$1
+
 # We expect all unit tests to pass
-meteor npm run test:unit:puppeteer 2> /dev/null
+meteor npm run test:unit:${DRIVER} 2> /dev/null
 UNIT_TEST_EXIT=$?
-echo "npm run test:unit:puppeteer exited with code $UNIT_TEST_EXIT"
+echo "npm run test:unit:$DRIVER exited with code $UNIT_TEST_EXIT (expected: 0)"
 
 if [ $UNIT_TEST_EXIT -ne 0 ]; then
   echo "expected exit code 0, got $UNIT_TEST_EXIT; exit 1"
@@ -14,9 +16,9 @@ if [ $UNIT_TEST_EXIT -ne 0 ]; then
 fi
 
 # We expect all app tests to fail
-meteor npm run test:app:puppeteer 2> /dev/null
+meteor npm run test:app:${DRIVER} 2> /dev/null
 APP_TEST_EXIT=$?
-echo "npm run test:app:puppeteer exited with code $APP_TEST_EXIT"
+echo "npm run test:app:$DRIVER exited with code $APP_TEST_EXIT (expected: 1)"
 
 if [ $APP_TEST_EXIT -ne 1 ]; then
   echo "expected exit code 1, got $APP_TEST_EXIT; exit 1"
