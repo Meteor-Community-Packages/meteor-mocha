bf7d8e17c09ef0bb8070d0efeb946131715c15e0
jankapunkt <jkuester@uni-bremen.de>
2026-05-12 09:40:25 +0200

    publish 3.4.0-rc.0

--- package/package.js
+++ package/package.js
@@ -3,7 +3,7 @@ Package.describe({
   summary: 'Run Meteor package or app tests with Mocha',
   git: 'https://github.com/meteortesting/meteor-mocha.git',
   documentation: '../README.md',
-  version: '3.4.0',
+  version: '3.4.0-rc.0',
   testOnly: true,
 });
 
